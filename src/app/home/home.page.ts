209defb8470b06b649de2fed95c4aca134a2f451
smitamusale30 <smitamusale30@gmail.com>
2021-06-08 12:08:00 +0530

    completed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  todoList= [
+    {
+      itemName : 'learn the SCSS',
+      itemDueDate : '21-june-2021',
+      itemPriority: 'high',
+      itemCategory: 'Learning new thing'
+    },
+    {
+      itemName : 'Shopping',
+      itemDueDate : '22-june-2021',
+      itemPriority: 'low',
+      itemCategory: 'personal'
+    },
+    {
+      itemName : 'Complate the Module',
+      itemDueDate : '10-june-2021',
+      itemPriority: 'high',
+      itemCategory: 'officework'
+    },
+    {
+      itemName : 'Learn the Ionic',
+      itemDueDate : '29-june-2021',
+      itemPriority: 'middle',
+      itemCategory: 'Learning new thing'
+    }
+  ];
+
+  today : number = Date.now();
   constructor() {}
 
 }
